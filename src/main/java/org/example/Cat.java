f99f995e993abf99dafcfe31cfd5d6b9bfbf2003
a.yudin <a.yudin@bft.ru>
2025-03-17 13:26:20 +0300

    homework3

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Cat {
+    public int age;
+    public String name;
+    private static int count;
+
+    public Cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+        count++;
+        System.out.println("Новая кошка добавлена");
+    }
+
+    public void checkCats(){
+        System.out.println("Кошек добавлено " + count);
+    }
+}
