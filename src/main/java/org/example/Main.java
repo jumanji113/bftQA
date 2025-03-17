70f50107f70b59aaff5e0cb7d6f8a382e604fb9e
a.yudin <a.yudin@bft.ru>
2025-03-17 13:26:20 +0300

    homework3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cat cat1 = new Cat(10, "Pushok");
+        Cat cat2 = new Cat(10, "Matroskin");
+        Cat cat3 = new Cat(10, "Barbos");
+        cat1.checkCats();
     }
 }
