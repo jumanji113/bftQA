242eb4ac33c4be70516b9a56759712e0ae1bec32
a.yudin <a.yudin@bft.ru>
2025-04-18 12:20:39 +0300

    refactoring java 11

--- src/test/java/uiTest/FirstUi.java
+++ src/test/java/uiTest/FirstUi.java
@@ -1,11 +1,9 @@
 package uiTest;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selenide.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.$x;
 
 
 public class FirstUi {
@@ -13,7 +11,7 @@ public class FirstUi {
     @Test
     @DisplayName("Проверка поиска и иконки яндекса")
     public void firstTest(){
-        Selenide.open("https://ya.ru/");
+        open("https://ya.ru/");
         $x("//input[@placeholder='Найдётся всё']").setValue("bft");
         $x("//button[text()='Найти']").click();
         $x("//a[@class='HeaderLogo']").shouldBe(Condition.visible);
